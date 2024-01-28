b7eb9029d4c9fbdd92effb2381b8124e950b8fb9
vaibhavkanawade <vaibhavkanawade12@gmail.com>
2024-01-28 23:58:04 +0530

    notes

--- notes.js
+++ notes.js
@@ -1,1 +1,9 @@
 //what is javascipt
+//javascipt is high level interpreted programming language
+//Prilimary use for fronted development 
+
+//what is high level
+//high level means easy to understands and deals with loop and function
+
+//what is interpreted
+//interpreted means not comiple before it can run
