5b4ad2593e38ccdd0a622214a7f9c6a85eefccfe
vaibhavkanawade <vaibhavkanawade12@gmail.com>
2024-01-30 12:48:34 +0530

    check palindrome in array

---
+++ array/palindromeArr.js
@@ -0,0 +1,23 @@
+function checkpalindrome(str) {
+    const len = str.length;
+    for (let i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function filterPal(arr) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (checkpalindrome(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+
+let arr = ["madam", 121, "sir"];
+console.log(filterPal(arr));
