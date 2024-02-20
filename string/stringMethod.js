fd6ea41c48f69d227eee217fd2a7171464f09602
vaibhavkanawade <vaibhavkanawade12@gmail.com>
2024-02-20 22:09:10 +0530

    string inbulit opration used

---
+++ string/stringMethod.js
@@ -0,0 +1,98 @@
+// let str = " he is good person he is good";
+// let part = str.substring(5,-2);
+// console.log (part)
+// let part = str.length;
+// let part = str.substring(1,-2);
+//-negative value consider a value is 0 in substring
+
+// let part = str.replace("he","she");
+// let part = str.replace(/He/gi,"she");
+// g used for all and i used for noncase sensitive
+// let part = str.concat("hello");
+// let part = "hello"+str;
+// let ch = str.indexOf("ood");
+//***********concat 
+// let newstr = str.slice(0,str.indexOf('d')+1)+ " Hello"+str.slice(str.indexOf('d')+1);
+// console.log(newstr);
+
+//newest metod conact
+// let v = "hello";
+// let str = `he is ${v} good boy`;
+// console.log(str);
+let str = "hello sir";
+console.log(str.trim());
+// let str = "Today is a beautiful day. Let us enjoy today and not worry about tomorrow! Today will not repeat itself";
+// let arr = str.split(/today/gi);
+// console.log(arr);
+// function checkPalindrome(string) {
+
+    
+//     const len = string.length;
+
+//     for (let i = 0; i < len / 2; i++) {
+//      if (string[i] !== string[len - 1 - i]) {
+//             console.log( 'It is not a palindrome');
+//         }
+//     }
+//    console.log('It is a palindrome');
+// }
+
+// const string ="aba";
+
+// const value = checkPalindrome(string);
+
+// console.log(value);
+// let str="aba";
+// let str1 ='';
+// for(let i=0;i<str.length;i++){
+// let ch=str.charAt(i);
+// str1=ch+str1;
+// }
+// console.log("string is"   +  str1);
+// if(str===str1){
+//     console.log("string is palindrome");
+// }
+// else{
+//     console.log("string is not plaindrome");
+// }
+// let arr = [1, 2, 10, 4, 6];
+
+ 
+
+// const res = arr.sort((a,b) => a-b);
+
+ 
+
+// console.log(res);
+// function findMinMax() {
+//     let Arr = [50, 60, 20, 10, 40];
+ 
+//     let minValue = Math.min(...Arr);
+//     let maxValue = Math.max(...Arr);
+     
+//     console.log("Minimum element is:" + minValue);
+//     console.log("Maximum Element is:" + maxValue);
+// }
+ 
+//  findMinMax();
+
+// let str = "Today is a beautiful day. Let us enjoy today and not worry about tomorrow! Today will not repeat itself";
+// let arr = str.split(/today/gi);
+// console.log(arr);
+
+// let Even = " ";
+// let n = 101;    
+// for(let i = 1; i <= n; i++)
+//   Even = !Even;
+       
+//   console.log(Even);
+
+// if (Even(n))
+// document.write("Even");
+// else
+// document.write("Odd");
+
+
+// let str = "aaaaaaahello";
+// let c = 'a';
+// console.log(str.split(c).length - 1);
